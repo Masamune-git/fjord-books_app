46013d28032e7d993b2c30249fb5e572483d8ae2
gentosato <2396niisankyumu@gmail.com>
2021-05-08 01:58:05 +0900

    多言語(英語と日本語)化対応実施
    /config/locales/ のja.yml en.ymlファイルを編集
    app/views/books の各ファイルに日本語を表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,15 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
 
-    def set_locale
-        I18n.locale = locale
-    end
+  def set_locale
+    I18n.locale = locale
+  end
 
-    def locale
-        @locale ||= params[:locale] ||= I18n.default_locale
-    end
+  def locale
+    @locale ||= params[:locale] ||= I18n.default_locale
+  end
 
-    def default_url_options(options={})
-      options.merge(locale: locale)
+  def default_url_options(options = {})
+    options.merge(locale: locale)
   end
 end
