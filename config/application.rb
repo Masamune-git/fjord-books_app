383139f525dc83b1200f84bb1d3cee4a111e027c
gentosato <2396niisankyumu@gmail.com>
2021-05-08 01:37:11 +0900

    ja.ymlファイルとindex.html.erbファイルを日本語化対応

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,10 @@ module BooksApp
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.i18n.default_locale = :ja
+    
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
 
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:ja, :en]
 
   end
 end
