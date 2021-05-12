feefb197f55e7b8216a3ba906cf67c4b274a0f2d
gentosato <2396niisankyumu@gmail.com>
2021-05-13 05:01:49 +0900

    郵番号、住所、自己紹介をサインアップ、更新時に反映されるように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :postcode, presence: true
+  validates :address_city, presence: true
+  validates :self_introduction, presence: true
 end
