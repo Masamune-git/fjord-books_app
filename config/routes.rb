67d079f81eb3ad87e8cb92e84193d0e468405865
gentosato <2396niisankyumu@gmail.com>
2021-05-13 02:02:17 +0900

    ユーザーの一覧画面と詳細画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users
+  resources :users, :only => [:index, :show]
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html  
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
