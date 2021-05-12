a731c6b42f0162ae0860cd61da4f10c90ade91b6
gentosato <2396niisankyumu@gmail.com>
2021-05-13 04:35:20 +0900

    ユーザー情報の変更で郵便峰号、住所、自己紹介の変更が反映されるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:postcode])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:address_city])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:postcode])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:address_city])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:self_introduction])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :postcode, presence: true
+  validates :address_city, presence: true
+  validates :self_introduction, presence: true
 end
