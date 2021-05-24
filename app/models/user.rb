90e3ce04518cbc6f13d24e1899017c3395fc9896
gentosato <2396niisankyumu@gmail.com>
2021-05-24 18:26:18 +0900

    フォロー、フォロワー一覧とフォロー、フォロー解除ボタンの設置

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,19 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
     end
   end
+
+    # ユーザーをフォローする
+    def follow(other_user)
+      following << other_user
+    end
+  
+    # ユーザーをフォロー解除する
+    def unfollow(other_user)
+      active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+  
+    # 現在のユーザーがフォローしてたらtrueを返す
+    def following?(other_user)
+      following.include?(other_user)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -68,9 +68,17 @@ User.transaction do
   end
 end
 
-User.order(:id).each do |user|
-  image_url = Faker::Avatar.image(slug: user.email, size: '150x150')
-  user.avatar.attach(io: URI.parse(image_url).open, filename: 'avatar.png')
-end
+# User.order(:id).each do |user|
+#   image_url = Faker::Avatar.image(slug: user.email, size: '150x150')
+#   user.avatar.attach(io: URI.parse(image_url).open, filename: 'avatar.png')
+# end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
 
 puts '初期データの投入が完了しました。' # rubocop:disable Rails/Output
