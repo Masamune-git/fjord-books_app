c88b22f4758b8ba8c4f921e2297619da9c6a9b4e
gentosato <2396niisankyumu@gmail.com>
2021-05-25 19:15:03 +0900

    複数行の要素を、一行一つに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
          :omniauthable, omniauth_providers: %i[github]
 
   def self.create_unique_string
