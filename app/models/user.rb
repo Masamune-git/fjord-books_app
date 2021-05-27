4abc2b277741104819260339952aeef4200fa769
gentosato <2396niisankyumu@gmail.com>
2021-05-27 20:04:01 +0900

    不要なファイルの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[github]
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :omniauthable,
+         omniauth_providers: %i[github]
 
   has_one_attached :avatar
 
