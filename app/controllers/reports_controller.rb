b2372be1cf788411bc44ace3d1a4be542b8b76e7
gentosato <2396niisankyumu@gmail.com>
2021-05-30 02:43:21 +0900

    reportのCRUDを設定

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,21 +1,48 @@
 class ReportsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :set_report, only: %i[edit update destroy]
+
   def new
     @report = Report.new
   end
 
+  def edit; 
+    @report = Report.find(params[:id])
+  end
+
   def create
-    @reoprt = Reoprt.new(reoprt_params)
-    @reoprt.user_id = current_user.id
-    @reoprt.save
-    redirect_to reoprts_path
+    @report = Report.new(report_params)
+    @report.user_id = current_user.id
+    @report.save
+    redirect_to reports_path
   end
 
   def index
-    @reports = Report.all
+    @reports = Report.order(:id).page(params[:page])
   end
 
   def show
     @report = Report.find(params[:id])
+  end
+
+  def destroy
+    @report.destroy
+  end
+
+  def update
+    if @report.update(report_params)
+      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit
+    end
+  end
+
+  private
+  
+  def set_report
+    @report = current_user.reports.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(:title, :body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -60,11 +60,10 @@ ActiveRecord::Schema.define(version: 2021_05_29_144800) do
 
   create_table "reports", force: :cascade do |t|
     t.string "title", null: false
-    t.string "body", null: false
+    t.text "body", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_reports_on_user_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
