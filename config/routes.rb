293075936fdaf675c6f3c7bfebad1c6383a2361a
gentosato <2396niisankyumu@gmail.com>
2021-06-01 00:57:00 +0900

    コントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,12 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "users/registrations" }
   root to: 'books#index'
-  resources :books
-  resources :reports
+  resources :books do
+    resource :comments, only: %i[create destroy]
+  end
+  resources :reports do
+    resource :comments, only: %i[create destroy]
+  end
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]
     scope module: :users do
