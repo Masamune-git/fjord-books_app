67b362f4ca16d38ec115246cf10b749db97086e8
gentosato <2396niisankyumu@gmail.com>
2021-06-03 19:58:36 +0900

    seeds.rbの記述を修正
    relationshipsのmigrateフィルの重複したindexの記載を修正

--- db/migrate/20210524060438_create_relationships.rb
+++ db/migrate/20210524060438_create_relationships.rb
@@ -5,11 +5,8 @@ class CreateRelationships < ActiveRecord::Migration[6.1]
     create_table :relationships do |t|
       t.integer :following_id, null: false
       t.integer :follower_id, null: false
-
       t.timestamps
     end
-    add_index :relationships, :following_id
-    add_index :relationships, :follower_id
     add_index :relationships, %i[following_id follower_id], unique: true
   end
 end
