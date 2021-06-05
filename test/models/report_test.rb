f6f56db4e8e9920a3149e02f04f534163c0dbf86
gentosato <2396niisankyumu@gmail.com>
2021-06-06 06:28:18 +0900

    report_test.rb にeditable?メソッドのテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -5,11 +5,14 @@ require 'test_helper'
 class ReportTest < ActiveSupport::TestCase
   setup do
     @report = reports(:alice_report)
+    @alice = users(:alice)
+    @bob = users(:bob)
   end
-  test "#created_on" do
+  test '#created_on' do
     assert_equal Date.current, @report.created_on
   end
-  test "#created_on" do
-    assert_equal Date.current, @report.created_on
+  test '#editable?' do
+    assert @report.editable?(@alice)
+    assert_not @report.editable?(@bob)
   end
 end
