a4b4cc6d4a44412fcf555c10161e9a2b41a0eccb
gentosato <2396niisankyumu@gmail.com>
2021-06-06 01:40:16 +0900

    user_test.rb にname_or_emailメソッドのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,13 +3,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "#name_or_email" do
+    user = User.new(email: "foo@example.com", name: "")
+    assert_equal "foo@example.com", user.name_or_email
+
+    user.name = "Foo Bar"
+    assert_equal "Foo Bar", user.name_or_email
   end
 
-  # test "name_or_email" do
+  # test "#unfollow" do
   #   user = User.new(email: "foo@example.com", name: "")
   #   assert_equal "foo@example.com", user.name_or_email
+
+  #   user = User.new(email: "hoge@example.com", name: "hoge")
+  #   assert_equal "hoge", user.unfollow(user)
   # end
 
 end
