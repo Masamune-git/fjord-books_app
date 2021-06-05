a68c695e2b5eafe950fb151ae4e8737f4eadeed3
gentosato <2396niisankyumu@gmail.com>
2021-06-06 06:12:19 +0900

    report_test.rb にcreated_onメソッドのテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @report = reports(:alice_report)
+  end
+  test "#created_on" do
+    assert_equal Date.current, @report.created_on
+  end
+  test "#created_on" do
+    assert_equal Date.current, @report.created_on
+  end
 end
