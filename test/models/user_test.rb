7fb31f1e03c903b7380bf31021e5f8d445cbf987
gentosato <2396niisankyumu@gmail.com>
2021-06-06 03:35:57 +0900

    user_test.rb にfollowing?メソッドとfollowed_by?メソッドのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,33 +6,42 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @me = User.create!(email: "me@example.com", password: "password")
+    @she = User.create!(email: "she@example.com", password: "password")
+  end
 
-  test "#name_or_email" do
+
+  test "#name_or_email" do  
     user = User.new(email: "foo@example.com", name: "")
     assert_equal "foo@example.com", user.name_or_email
-
     user.name = "Foo Bar"
     assert_equal "Foo Bar", user.name_or_email
   end
 
-  test "#follow" do
-    me = User.create!(email: "me@example.com", password: "password")
-    she = User.create!(email: "she@example.com", password: "password")
-    
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
+  test "#follow" do  
+    assert_not @me.following?(@she)
+    @me.follow(@she)
+    assert @me.following?(@she)
   end
 
   test "#unfollow" do
-    me = User.create!(email: "me@example.com", password: "password")
-    she = User.create!(email: "she@example.com", password: "password")
+    assert_not @me.following?(@she)
+    @me.follow(@she)
+    assert @me.following?(@she)
     
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
-    
-    me.unfollow(she)
-    assert_not me.following?(she)
+    @me.unfollow(@she)
+    assert_not @me.following?(@she)
   end
+
+  test "#following?" do  
+    Relationship.create!(following_id: @she.id, follower_id: @me.id)
+    assert @me.following?(@she)
+  end
+
+  test "#followed_by?" do  
+    Relationship.create!(following_id: @she.id, follower_id: @me.id)
+    assert @she.followed_by?(@me)
+  end
+
 end
