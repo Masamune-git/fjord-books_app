1b44fc8ad19edaaf50cab95edee30a2ba3800c3b
gentosato <2396niisankyumu@gmail.com>
2021-06-06 01:54:59 +0900

    user_test.rb にfollowメソッドのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,12 +15,13 @@ class UserTest < ActiveSupport::TestCase
     assert_equal "Foo Bar", user.name_or_email
   end
 
-  # test "#unfollow" do
-  #   user = User.new(email: "foo@example.com", name: "")
-  #   assert_equal "foo@example.com", user.name_or_email
-
-  #   user = User.new(email: "hoge@example.com", name: "hoge")
-  #   assert_equal "hoge", user.unfollow(user)
-  # end
+  test "#follow" do
+    me = User.create!(email: "me@example.com", password: "password")
+    she = User.create!(email: "she@example.com", password: "password")
+    
+    assert_not me.following?(she)
+    me.follow(she)
+    assert me.following?(she)
+  end
 
 end
