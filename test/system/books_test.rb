0647a7670721d88368759dd2b381582c241a69be
gentosato <2396niisankyumu@gmail.com>
2021-06-07 03:01:37 +0900

    system/books_test.rb 日報の新規作成実装

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,7 +4,7 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    # @book = books(:one)
+    # @reports = reports(:alice_report)
     visit root_url
     fill_in 'Eメール', with: 'alice@example.com'
     fill_in 'パスワード', with: 'password'
@@ -17,28 +17,31 @@ class BooksTest < ApplicationSystemTestCase
     assert_selector 'h1', text: '本'
   end
 
-  # test 'creating a Book' do
-  #   visit books_url
-  #   click_on 'New Book'
+  test 'creating a Report' do
+    click_on '日報'
+    click_on '新規作成'
 
-  #   fill_in 'Memo', with: @book.memo
-  #   fill_in 'Title', with: @book.title
-  #   click_on 'Create Book'
+    fill_in 'タイトル', with: '新しい日報です'
+    fill_in '内容', with: 'おはようございます'
+    click_button '登録する'
 
-  #   assert_text 'Book was successfully created'
-  #   click_on 'Back'
-  # end
+    assert_text '日報が作成されました。'
+    assert_text '新しい日報です'
+    assert_text 'おはようございます'
+    
+  end
 
-  # test 'updating a Book' do
-  #   visit books_url
-  #   click_on 'Edit', match: :first
+  # test 'updating a Report' do
+  #   click_on '編集'
+  #   click_on '新規作成'
 
-  #   fill_in 'Memo', with: @book.memo
-  #   fill_in 'Title', with: @book.title
-  #   click_on 'Update Book'
+  #   fill_in 'タイトル', with: '新しい日報です'
+  #   fill_in '内容', with: 'おはようございます'
+  #   click_button '登録する'
 
-  #   assert_text 'Book was successfully updated'
-  #   click_on 'Back'
+  #   assert_text '日報が作成されました。'
+  #   assert_text '新しい日報です'
+  #   assert_text 'おはようございます'
   # end
 
   # test 'destroying a Book' do
