d3e17c946a7d68d768181c131b67aaaff22aa588
gentosato <2396niisankyumu@gmail.com>
2021-06-07 05:41:50 +0900

    system/books_test.rb 日報の編集実装

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,7 +4,7 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    # @reports = reports(:alice_report)
+    @report = reports(:alice_report)
     visit root_url
     fill_in 'Eメール', with: 'alice@example.com'
     fill_in 'パスワード', with: 'password'
@@ -12,12 +12,8 @@ class BooksTest < ApplicationSystemTestCase
     click_button 'ログイン'
   end
 
-  test 'visiting the index' do
-    visit root_url
-    assert_selector 'h1', text: '本'
-  end
-
   test 'creating a Report' do
+    visit root_url
     click_on '日報'
     click_on '新規作成'
 
@@ -31,18 +27,21 @@ class BooksTest < ApplicationSystemTestCase
     
   end
 
-  # test 'updating a Report' do
-  #   click_on '編集'
-  #   click_on '新規作成'
+  test 'updating a Report' do
+    visit root_url
+    click_on '日報'
+    click_link '編集', href: edit_report_path(@report)
+    
+    assert_selector 'h1', text: '日報の編集'
 
-  #   fill_in 'タイトル', with: '新しい日報です'
-  #   fill_in '内容', with: 'おはようございます'
-  #   click_button '登録する'
+    fill_in 'タイトル', with: '編集した日報です'
+    fill_in '内容', with: 'ご飯を食べた'
+    click_button '更新する'
 
-  #   assert_text '日報が作成されました。'
-  #   assert_text '新しい日報です'
-  #   assert_text 'おはようございます'
-  # end
+    assert_text '日報が更新されました。'
+    assert_text '編集した日報です'
+    assert_text 'ご飯を食べた'
+  end
 
   # test 'destroying a Book' do
   #   visit books_url
