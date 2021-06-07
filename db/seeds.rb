2251bdbf1f7d1744749354897f10b41a292f4088
gentosato <2396niisankyumu@gmail.com>
2021-06-07 13:47:11 +0900

    seeds.rb のコメントアウトを解除

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,19 +68,19 @@ User.transaction do
   end
 end
 
-# User.order(:id).each do |user|
-#   image_url = Faker::Avatar.image(slug: user.email, size: '150x150')
-#   user.avatar.attach(io: URI.parse(image_url).open, filename: 'avatar.png')
-# end
+User.order(:id).each do |user|
+  image_url = Faker::Avatar.image(slug: user.email, size: '150x150')
+  user.avatar.attach(io: URI.parse(image_url).open, filename: 'avatar.png')
+end
 
 # User.destroy_all で全件削除されているはずだが念のため
 Relationship.destroy_all
 
 # 後輩が先輩を全員フォローする
-# User.order(id: :desc).each do |user|
-#   User.where('id < ?', user.id).each do |other|
-#     user.follow(other)
-#   end
-# end
+User.order(id: :desc).each do |user|
+  User.where('id < ?', user.id).each do |other|
+    user.follow(other)
+  end
+end
 
 puts '初期データの投入が完了しました。' # rubocop:disable Rails/Output
